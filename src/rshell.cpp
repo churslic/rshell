e29a65c29786e8d887c2e84ede388b984345acaf
Chelsey Calingo <ccali003@ucr.edu>
2015-01-23 19:35:18 -0800

    Added while loop to main function, exit function, and comments

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -5,12 +5,97 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <string>
+#include <vector>
 using namespace std;
 
+/**
+ * trans_string function
+ * @summary: transforms the string so that there are spaces in between
+ * the connectors
+ * @param: the entire command line
+ * @return: returns nothing
+ * **/
+void trans_string (string& command) {
+
+    for (string::iterator it = command.begin(); it != command.end(); ++it) {
+        if (*it == ';') {
+            it = command.insert(it, ' ');
+            ++it;
+            ++it;
+            it = command.insert(it, ' ');
+        }
+    }
+
+    for (string::iterator it = command.begin(); it != command.end(); ++it) {
+        if (*it == '&' && *(it + 1) == '&') {
+            it = command.insert(it, ' ');
+            ++it;
+            ++it;
+            ++it;
+            it = command.insert(it, ' ');
+        }
+    }
+
+    for (string::iterator it = command.begin(); it != command.end(); ++it) {
+        if (*it == '|' && *(it + 1) == '|') {
+            it = command.insert(it, ' ');
+            ++it;
+            ++it;
+            ++it;
+            it = command.insert(it, ' ');
+        }
+    }
+}
+
+
+/**
+ * execute function
+ * @summary: Uses execvp to call commands.
+ * @parameter: char **argv is command that wants to be used.
+ * @return: returns nothing
+ * **/
+bool execute (char **argv) {
+    bool exec_result = true;
+
+    int pid = fork();
+    if (pid == -1) {
+        perror("fork fail");
+        exit(1);
+    }
+    else if (pid == 0) {
+        int fail = 0;
+        fail = execvp(argv[0], argv);
+
+        if(fail == -1) {
+            perror("there was an error in execvp");
+        }
+
+        exec_result = false;
+        exit(1);
+    }
+    else {
+        if(wait(0) == -1) {
+            perror("wait() error");
+        }
+        return exec_result;
+    }
+}
+
+
+/**
+ * parse function
+ * @summary: Will take the command and store each string of characters into
+ * an array. strtok ignores all whitespace. Function will also break
+ * once # is found, since anything after that can be ignored. The function
+ * will also check for connectors and execute commands.
+ * @param: char *cmd is the entire line that was input. char **argv
+ * will be the array that stores each individual string of characters.
+ * @return: Does not return anything
+ * **/
 void parse (char *cmd, char **argv) {
     char *token = strtok(cmd, " \t\r\n");
     argv[0] = token;
-    //cout << "argv[0] = " << argv[0] << endl;
 
     int i = 1;
 
@@ -18,6 +103,8 @@ void parse (char *cmd, char **argv) {
         token = strtok(NULL, " \t\r\n");
         argv[i] = token;
         ++i;
+        ++size;
+
         if (token != NULL) {
             if (strcmp(token, "#") == 0) {
                 argv[i-1] = NULL;
@@ -27,38 +114,22 @@ void parse (char *cmd, char **argv) {
     }
 }
 
+
 int main(int argc, char **argv) {
     cout << "$ ";
-
-//    char cmd[100];
-//    cin.getline(cmd, 100);
-
     string command;
     getline(cin, command);
 
-    char *cmd = new char[command.length() + 1];
-    strcpy(cmd, command.c_str());
+    while(command != "exit") {
+        trans_string(command);
+        char *cmd = new char[command.length() + 1];
+        strcpy(cmd, command.c_str());
 
-// Parse cmd into argv
-    parse(cmd, argv);
+        parse(cmd, argv);
+        execute(argv);
 
-
-    int pid = fork();
-    if (pid == -1) {
-        perror("fork fail");
-    }
-    // This is the child process
-    else if (pid == 0) {
-        if(execvp(cmd, argv)) {
-            perror("there was an error in execvp");
-        }
-        exit(1);
-    }
-    // This is the parent process
-    else {
-        if(wait(0) == -1) {
-            perror("wait() error");
-        }
+        cout << "$ ";
+        getline(cin, command);
     }
 
     return 0;
