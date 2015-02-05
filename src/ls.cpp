e59f8622840171c06f4a496eac11dc9054fea18a
Chelsey Calingo <ccali003@ucr.edu>
2015-02-05 14:32:20 -0800

    Updated ls.cpp to print out alphabetically

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,60 +1,130 @@
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
 
 #include <iostream>
 #include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-void ls_a (DIR *dirp) {
-    dirent *direntp;
-    int i = 1;
-    while((direntp = readdir(dirp))) {
-        if (direntp == NULL && errno != 0) perror("readdir error");
-        printf("%-14s", direntp->d_name);
-        ++i;
 
-        if (i % 6 == 0) cout << endl;
-    }
-    cout << endl;
-    if (closedir(dirp) == -1) perror("closedir error");
+void print_l (struct stat &s) {
+    if(s.st_mode & S_IFDIR) cout << 'd';
+    else if(s.st_mode & S_IFLNK) cout << 'l';
+    else cout << '-';
 }
 
-void reg_ls (DIR *dirp) {
+void ls_l (DIR *dirp) {
+//int stat(const char *path, struct stat *buf);
     dirent *direntp;
-    int i = 1;
-    while((direntp = readdir(dirp))) {
-        if (direntp == NULL && errno != 0) perror("readdir error");
-        if (direntp->d_name[0] != '.') {
-            printf("%-14s", direntp->d_name);
-            ++i;
-        }
+    struct stat s;
+    vector<char *> v;
 
-        if (i % 6 == 0) cout << endl;
+    while((direntp = readdir(dirp))) {
+        if(stat(direntp->d_name, &s) == -1) perror("stat error");
+
     }
     cout << endl;
-    if (closedir(dirp) == -1) perror("closedir error");
+}
+
+vector<const char *> create_vec_ls_a(DIR *dirp) {
+    dirent *direntp;
+    vector<const char *> v;
+
+    const char *curr = ".";
+    const char *parent = "..";
+    v.push_back(curr);
+    v.push_back(parent);
+
+    while((direntp = readdir(dirp))) {
+        if(direntp == NULL && errno != 0) perror("readdir error");
+
+        if(strcmp(direntp->d_name, ".") != 0 &&
+           strcmp(direntp->d_name, "..") != 0) {
+            v.push_back(direntp->d_name);
+        }
+    }
+
+    vector<const char*>::iterator it = v.begin()+2;
+    sort(it, v.end());
+    return v;
+}
+
+void print_ls_a(DIR *dirp) {
+    vector<const char*> v = create_vec_ls_a(dirp);
+
+    int x = 1;
+    for(unsigned int i = 0; i < v.size(); ++i) {
+        printf("%-14s", v.at(i));
+        ++x;
+        if(x % 6 == 0) cout << endl;
+    }
+
+    if(x % 6 != 0) cout << endl;
+}
+
+vector<const char *> create_vec_ls(DIR *dirp) {
+    dirent *direntp;
+    vector<const char *> v;
+
+    while((direntp = readdir(dirp))) {
+        if(direntp == NULL && errno != 0) perror("readdir error");
+
+        if(direntp->d_name[0] != '.') {
+            v.push_back(direntp->d_name);
+        }
+    }
+
+    sort(v.begin(), v.end());
+    return v;
+}
+
+void print_ls(DIR *dirp) {
+    vector<const char *> v = create_vec_ls(dirp);
+
+    int x = 1;
+    for(unsigned int i = 0; i < v.size(); ++i) {
+        printf("%-14s", v.at(i));
+        ++x;
+        if(x % 6 == 0) cout << endl;
+    }
+
+    if(x % 6 != 0) cout << endl;
 }
 
 int main(int argc, char** argv) {
-    char const *dirName = ".";
+    //For the case that the only command would be "bin/ls"
+    if(argc == 1) {
+        const char *dirName = ".";
+        DIR *dirp = opendir(dirName);
+        if(dirp == NULL) perror("opendir error");
 
-    DIR *dirp = opendir(dirName);
-    if (dirp == NULL) perror("opendir error");
+        print_ls(dirp);
 
-    //reg_ls(dirp);
-    ls_a(dirp);
-
-    /*dirent *direntp;
-    while ((direntp = readdir(dirp))) {
-        if (direntp == NULL && errno != 0) perror("readdir error");
-        printf("%10s", direntp->d_name);
-        //cout << direntp->d_name;
+        if(closedir(dirp) == -1) perror("closedir error");
     }
-    cout << endl;
-    if (closedir(dirp) == -1) perror("closedir error");*/
+    //For the case that there may be flags or commands typed
+    //in after "bin/ls". Must first check if argv[1] has a flag
+    else {
+        if(argv[1][0] == '-') {
+            const char *dirName = ".";
+            DIR *dirp = opendir(dirName);
+            if(dirp == NULL) perror("opendir error");
+
+            print_ls_a(dirp);
+
+            if(closedir(dirp) == -1) perror("closedir error");
+        }
+        else {
+            cout << "FIX ME" << endl;
+        }
+    }
 
     return 0;
 }
